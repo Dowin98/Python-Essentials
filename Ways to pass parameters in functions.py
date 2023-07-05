e759b6ff1b5488c1683e0950be5b32e66bec2d70
Edwin M <dowin_98@outlook.com>
2023-07-05 17:59:37 -0500

    Create Ways to pass parameters in functions.py

---
+++ Ways to pass parameters in functions.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  4 19:40:56 2023
+
+@author: Dell
+"""
+#---------------------formas de pasar parametros-------------------------
+def resta(a,b):
+    print(a-b)
+    
+resta(5, 9)
+resta(a=3,b=4)
