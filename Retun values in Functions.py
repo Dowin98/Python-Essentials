793de9765c50a0e8f24c9943267286b1be086d5f
Edwin M <dowin_98@outlook.com>
2023-07-05 18:05:28 -0500

    how can use retuns in functions

---
+++ Retun values in Functions.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  4 19:43:27 2023
+
+@author: Dell
+"""
+
+#------------------------Almacenar valores de una funcion---------------------
+def multiply (x,y):
+    print(x*y)
+    return x*y #devolver un valor para utilizarla //simepre y cuando la variable 
+               #sea para utilizarla en diferentes operaciones 
+
+resultado=multiply(5, 25)
