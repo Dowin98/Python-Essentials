5fbf9800785d9f3589bb0f29b4c2b2a902a1b0b9
Edwin M <dowin_98@outlook.com>
2023-07-05 17:58:05 -0500

    User information by input

---
+++ 03_User informtion by input.py
@@ -0,0 +1,8 @@
+#--------------User Input ----------------------------
+name=input("Write your first name:")
+name2=input("Write your last name:")
+age=int(input("how old are you:"))#el input arroja una variable str por lo cual se le cambia a int
+address=input("your address: ")
+
+
+print("User information:",name,name2,age,address)
